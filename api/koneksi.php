8b6454a0568bb6021de2d272e2484342b7c20d5d
ImMRSM <sidikmaulan8272@gmail.com>
2026-04-13 19:52:22 +0700

    fix: full virtual tour logic and connection finalized

--- api/koneksi.php
+++ api/koneksi.php
@@ -1,13 +1,13 @@
 <?php
-$host = '://tidbcloud.com';
+$host = 'tidbcloud.com'; // JANGAN pakai :// di depannya
 $port = 4000;
-$user = '2TQDRPw3eFWrkqt.root'; // INI USER BARU
-$pass = 'TIy9LEkE59zNf5OC';     // INI PASSWORD BARU
+$user = '2TQDRPw3eFWrkqt.root'; 
+$pass = 'TIy9LEkE59zNf5OC'; 
 $db   = 'test';
 
 $conn = mysqli_init();
 
-// WAJIB: Gunakan SSL agar Vercel bisa konek ke TiDB Cloud
+// Wajib pakai SSL untuk TiDB Cloud
 mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
 
 if (!mysqli_real_connect($conn, $host, $user, $pass, $db, $port, NULL, MYSQLI_CLIENT_SSL)) {
