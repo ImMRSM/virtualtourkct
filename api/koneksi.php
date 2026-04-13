cc9cc35bd57aac7dbfae7a5bc78b917aa6f8122f
ImMRSM <sidikmaulan8272@gmail.com>
2026-04-13 20:04:23 +0700

    Perbaikan koneksi SSL TiDB

--- api/koneksi.php
+++ api/koneksi.php
@@ -1,5 +1,5 @@
 <?php
-$host = 'tidbcloud.com'; // JANGAN pakai :// di depannya
+$host = 'gateway01.ap-southeast-1.prod.aws.tidbcloud.com';
 $port = 4000;
 $user = '2TQDRPw3eFWrkqt.root'; 
 $pass = 'TIy9LEkE59zNf5OC'; 
@@ -7,10 +7,14 @@ $db   = 'test';
 
 $conn = mysqli_init();
 
-// Wajib pakai SSL untuk TiDB Cloud
+// --- BARIS INI WAJIB ADA UNTUK TiDB CLOUD ---
 mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
+// --------------------------------------------
 
 if (!mysqli_real_connect($conn, $host, $user, $pass, $db, $port, NULL, MYSQLI_CLIENT_SSL)) {
-    die("Koneksi gagal: " . mysqli_connect_error());
+    echo "Koneksi Gagal: " . mysqli_connect_error();
+    echo "<br>Error Code: " . mysqli_connect_errno();
+} else {
+    echo "Hore! Koneksi ke TiDB Cloud Berhasil.";
 }
 ?>
