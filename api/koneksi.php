a4547126832de981f8ba786467bf7fde77897bc5
ImMRSM <sidikmaulan8272@gmail.com>
2026-04-13 20:08:35 +0700

    fix username case sensitive

--- api/koneksi.php
+++ api/koneksi.php
@@ -1,19 +1,20 @@
 <?php
 $host = 'gateway01.ap-southeast-1.prod.aws.tidbcloud.com';
 $port = 4000;
-$user = '2TQDRPw3eFWrkqt.root'; 
+$user = '2TQDrPw3eFWrkqt.root'; // Cek huruf r-nya
 $pass = 'TIy9LEkE59zNf5OC'; 
 $db   = 'test';
 
 $conn = mysqli_init();
 
-// --- BARIS INI WAJIB ADA UNTUK TiDB CLOUD ---
+// TiDB Cloud butuh ini untuk keamanan
 mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
-// --------------------------------------------
 
-if (!mysqli_real_connect($conn, $host, $user, $pass, $db, $port, NULL, MYSQLI_CLIENT_SSL)) {
-    echo "Koneksi Gagal: " . mysqli_connect_error();
-    echo "<br>Error Code: " . mysqli_connect_errno();
+// Eksekusi koneksi
+$koneksi = @mysqli_real_connect($conn, $host, $user, $pass, $db, $port, NULL, MYSQLI_CLIENT_SSL);
+
+if (!$koneksi) {
+    die("Koneksi Gagal: " . mysqli_connect_error());
 } else {
     echo "Hore! Koneksi ke TiDB Cloud Berhasil.";
 }
